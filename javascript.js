e40a5211f5dbd1ef6aea00b3770e5c2216e2df2c
rogcald1 <roger@justworks.com>
2021-02-10 16:15:04 -0500

    made a couple of changes, such as removing the alert to add a task, allowing enter key to be hit for task addition, adjusting how the notasks/gettowork message appears, and some other cleaning up. also added a few css tweaks to get the color scheme to match up

--- javascript.js
+++ javascript.js
@@ -1,37 +1,65 @@
-//doing this so task lines have a specific selector if needed
+// doing this so task lines have a specific selector if needed
 let listNum = 0;
 let taskCount = 0;
+
+// below lines needed to help with the error message that pops up/disappears depending on user input
+let newLabel = document.createElement('p');
+newLabel.setAttribute('class','labelForBox');
+newLabel.textContent='';
+document.querySelector('#labelInput').appendChild(newLabel);
+let inputTask = document.getElementById('inputText');
+
+// allowing users to hit enter to add tasks
+inputTask.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        addTask();
+    }
+}) 
 
 //adding task to the list, along with the checkbox and trashcan img
 function addTask() {
     listNum += 1;
-    let task = prompt('what is your task?');
+    let task1 = document.querySelector('.inputText').value;
+
+    //checks if there is a blank value or if the request to input a task was already made
+    if ((task1 !== '' && newLabel.textContent === 'please input a task :)') || (task1 !== '' && newLabel.textContent === ''))
+    {
+
+    document.querySelector('.inputText').value = '';
+    document.querySelector('.labelForBox').textContent = '';
     //checkbox
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('id',`divitem${listNum}`);
+    newDiv.setAttribute('class',`taskDiv`);
+    document.querySelector(`#ultList`).appendChild(newDiv);
+
     let newItem = document.createElement('input');
     newItem.type = "checkbox";
     newItem.setAttribute("id",`item${listNum}`);
     newItem.setAttribute("class",'aCheckbox');
     newItem.setAttribute("onclick",`crossTask("item${listNum}")`)
-    document.querySelector(`#tasklist`).appendChild(newItem);
+    document.querySelector(`#divitem${listNum}`).appendChild(newItem);
     //text
     let newLabel = document.createElement('p');
     newLabel.setAttribute("id",`inditem${listNum}`);
     newLabel.setAttribute("class",'aTask');
-    newLabel.textContent = `${task}`;
-    document.querySelector('#tasklist').appendChild(newLabel);
+    newLabel.textContent = `${task1}`;
+    document.querySelector(`#divitem${listNum}`).appendChild(newLabel);
     //trash
     let newTrash = document.createElement('img');
     newTrash.setAttribute("src","img/2021_01_20_0gu_Kleki.png");
     newTrash.setAttribute("id",`trashitem${listNum}`);
+    newTrash.setAttribute("class",`aTrashcan`);
     newTrash.setAttribute("onclick",`removeTask("item${listNum}")`);
-    document.querySelector('#tasklist').appendChild(newTrash);
-    //line breaks that also need to be removed later on when trash is clicked
-    let newBreak = document.createElement('br');
-    newBreak.setAttribute("id",`britem${listNum}`);
-    document.querySelector('#tasklist').appendChild(newBreak);
-
+    document.querySelector(`#divitem${listNum}`).appendChild(newTrash);
+    
     taskCount += 1;
     taskCheck();
+    } else if ((task1 === '') && (newLabel.textContent === '')) {
+        newLabel.textContent = 'please input a task :)';
+        document.querySelector('#labelInput').appendChild(newLabel);
+    } else if ((task1 === '') && (newLabel.textContent === 'please input a task :)')){
+    }
 }
 
 //crosses out the task once checkbox is completed
@@ -50,15 +78,15 @@ function crossTask(task) {
 
 //removes task once trashcan is clicked
 function removeTask(task) {
-    let indTask = document.getElementById(`ind${task}`);
-    let indBox = document.getElementById(`${task}`);
-    let indTrash = document.getElementById(`trash${task}`);
-    let indBreak = document.getElementById(`br${task}`);
-    
-    indTask.remove()
-    indBox.remove()
-    indTrash.remove()
-    indBreak.remove()
+    let remTrash = document.querySelector(`#trash${task}`)
+    remTrash.removeAttribute('onclick');
+
+    let indTask = document.querySelector(`#div${task}`)
+    indTask.classList.add("removed");
+    indTask.addEventListener("transitionend", () =>  
+        { 
+    indTask.remove(); 
+    })
 
     taskCount -= 1;
     taskCheck();
@@ -70,13 +98,13 @@ function taskCheck() {
         if (fText === null) {
             return;
         } else {
-            fText.remove();
+            fText.textContent = "let's get to work!";
         }
-    } else {
-        let fText = document.createElement('p');
-        fText.textContent = "no tasks :D";
+    } else if (taskCount <= 0){
+        taskCount = 0;
+        let fText = document.getElementById('frontText');
+        fText.textContent = "no tasks! :D";
         fText.setAttribute("id","frontText");
-        document.querySelector("#tasklist").appendChild(fText);
     }
 }
 
